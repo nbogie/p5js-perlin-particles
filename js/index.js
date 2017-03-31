0c440658c03a9c52b17fb410429f2724f5c493cb
Neill Bogie <neillbogie@googlemail.com>
2017-03-31 09:40:13 +0100

    graw lines.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,60 @@
    'use strict';
    var gParticles = [];
    var gStep = 50;
+   var gNumCols = 10;
+   var gNumRows = 10;
 
    var Particle = function() {
      this.pos = createVector(random(width), random(height));
+     this.prevPos = this.pos.copy();
      this.vel = p5.Vector.random2D().mult(0.1 * random());
      this.acc = createVector();
+     this.updatePrevX = function() {
+       this.prevPos.x = this.pos.x;
+     }
+     this.updatePrevY = function() {
+       this.prevPos.y = this.pos.y;
+     }
      this.update = function() {
+       this.updatePrevX();
+       this.updatePrevY();
+
        this.vel.add(this.acc);
-       this.vel.limit(4);
+       this.vel.limit(gOpts.maxSpeed);
        this.pos.add(this.vel);
+
        if (this.pos.x < 0) {
          this.pos.x = width;
+         this.updatePrevX();
        } else {
          if (this.pos.x > width) {
            this.pos.x = 0;
+           this.updatePrevX();
          }
        }
        if (this.pos.y < 0) {
          this.pos.y = height;
+         this.updatePrevY();
+
        } else {
          if (this.pos.y > height) {
            this.pos.y = 0;
+           this.updatePrevY();
          }
        }
        this.acc.mult(0);
      }
+     
      this.show = function() {
        fill(gOpts.particleColor);
-       noStroke();
-       rect(this.pos.x, this.pos.y, 2, 2);
+       if (gOpts.drawLines) {
+         strokeWeight(0.1);
+         stroke(gOpts.particleColor);
+         line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y);
+       } else {
+         noStroke();
+         rect(this.pos.x, this.pos.y, 2, 2);
+       }
      }
    }
 
@@ -37,35 +62,47 @@
      this.animSpeed = 0.001;
      this.change = 0.04;
      this.drawForces = false;
-     this.numParticles= 3000;
-     this.lineColor = [255, 255, 255];
-     this.particleColor = [255,105,0,20];
+     this.maxSpeed = 4.0;
+     this.numParticles = 3000;
+     this.strength = 0.51;
+     this.drawLines = true;
+     this.backgroundColor = [0, 0, 30, 5];
+     this.lineColor = [255, 255, 255];     
+     this.particleColor = [255, 105, 0, 20];
      this.reseed = function() {
        reseed();
      };
      this.restart = restart;
    }
    var gOpts; //params model shared between gui and simulation.
+   function nearestMult(n, x) {
+     var rem = n % x;
+     return n - rem;
+   }
 
    function setup() {
      //createCanvas(200, 200); //windowWidth, windowHeight);
      frameRate(60);
-     createCanvas(windowWidth, windowHeight);
+     createCanvas(nearestMult(windowWidth, gStep), nearestMult(windowHeight, gStep));
      gOpts = new Options();
      var gui = new dat.GUI();
      gui.addColor(gOpts, 'lineColor');
+     gui.addColor(gOpts, 'backgroundColor');     
      gui.addColor(gOpts, 'particleColor');
      gui.add(gOpts, 'animSpeed', 0, 0.003);
      gui.add(gOpts, 'change', 0.00001, 0.1);
+     gui.add(gOpts, 'maxSpeed', 0.1, 10);
+     gui.add(gOpts, 'drawLines');
      gui.add(gOpts, 'numParticles', 1, 4000);
+     gui.add(gOpts, 'strength', 0, 4);
      gui.add(gOpts, 'reseed');
      gui.add(gOpts, 'restart');
      gui.add(gOpts, 'drawForces');
+     dat.GUI.toggleHide();
      restart();
    }
 
-   function mousePressed() {
-   }
+   function mousePressed() {}
 
    function restart() {
      reseed();
@@ -89,6 +126,7 @@
      noStroke();
      rect(x, y, step, step);
      push();
+     strokeWeight(1);
      stroke(gOpts.lineColor);
      var halfStep = step / 2;
      translate(x + halfStep, y + halfStep);
@@ -101,18 +139,23 @@
    var gForces = [];
 
    function draw() {
-     background(color(0, 0, 30, 1));
+     background(gOpts.backgroundColor);
 
      fill(0, 0, 255);
      var change = gOpts.change;
      var animSpeed = gOpts.animSpeed;
      gForces = [];
-     for (var col = 0; col < width / gStep; col += 1) {
-       for (var row = 0; row < height / gStep; row += 1) {
+     gNumCols = floor(width / gStep);
+     gNumRows = floor(height / gStep);
+
+     for (var col = 0; col < gNumCols; col++) {
+       for (var row = 0; row < gNumRows; row++) {
+
          var x = col * gStep;
          var y = row * gStep;
-         var n = noise(animSpeed * frameCount + change * col, animSpeed * frameCount + change * row);
-         gForces[col + width * row] = n;
+         var n = noise(animSpeed * frameCount + change * col,
+           animSpeed * frameCount + change * row);
+         gForces[col + gNumCols * row] = n;
          if (gOpts.drawForces) {
            drawAngle(x, y, gStep, n);
          }
@@ -124,19 +167,15 @@
        p.update();
        p.show();
      });
-
-     //noLoop();
    }
 
    function applyNearestForceToParticle(p) {
      var gridX = floor(p.pos.x / gStep);
      var gridY = floor(p.pos.y / gStep);
-     var ix = gridX + width * gridY;
+     var ix = gridX + floor(width / gStep) * gridY;
 
      var nearestForce = gForces[ix];
-
-     // console.log("ix: "+ ix +", gridX,gridY: "+[gridX, gridY]+"near: "+nearestForce + " gforcessize: " + gForces.length);
      var angle = angleForNoiseValue(nearestForce);
      var angleVec = p5.Vector.fromAngle(angle);
-     p.acc.add(angleVec.mult(0.51));
+     p.acc.add(angleVec.mult(gOpts.strength));
    }
